f064356054e9b5524b83837cc3b1e66e2dd16f74
HyeokHam <kn7137@gmail.com>
2023-02-03 01:05:23 +0900

    23. 02. 03

--- Algorithm/Python/programmers/7_count.py
+++ Algorithm/Python/programmers/7_count.py
@@ -3,5 +3,20 @@ def solution(array):
     ''.join(array)
     return array.count('7')
 
+'''
+다른 사람 풀이
+'''
+
+# def solution(array):
+#     return str(array).count('7')
+
+# list를 str로 바꾸면 ''.join()을 안해도 자동으로 합쳐진다.
+
+# def solution(array):
+#     return ''.join(map(str, array)).count('7')
+
+# 만약 ''.join()을 사용하고 싶다면 map을 통해 문자열로 바꿔주자
+# map 함수는 파이썬 내장 함수로 여러 개의 데이터를 한 번에 다른 형태로 변환할 때 사용함
+
 array = list(map(int, input().split()))
 print(solution(array=array))
--- Algorithm/Python/programmers/string_split.py
+++ Algorithm/Python/programmers/string_split.py
@@ -11,3 +11,7 @@ def solution(my_str, n):
 
 # range 도 step이 존재함 즉, range(0, 12, 6) 이면 0 ~ 11 까지 6씩 증가하므로 0, 6에 해당된다. (7 ~ 11 까지는 6에 못미치므로 잘림)
 # 또한 슬라이싱은 인덱스를 초과해도 에러가 나지 않는다.
+
+my_string = input()
+n = int(input())
+print(solution(my_str=my_string, n=n))
