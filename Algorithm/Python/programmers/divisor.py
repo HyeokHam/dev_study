f28e8b946476b797e9725fba3105142a20ae843b
HyeokHam <kn7137@gmail.com>
2023-02-10 18:01:49 +0900

    23. 02. 10

--- Algorithm/Python/programmers/divisor.py
+++ Algorithm/Python/programmers/divisor.py
@@ -2,5 +2,6 @@ def solution(n):
     answer = [n//i for i in reversed(range(1, n+1)) if n % i == 0]
     return answer
 # reversed 대신 range(n+1, 0, -1) 로 하면 똑같은 결과가 나온다.
+
 n = int(input())
 print(solution(n=n))
